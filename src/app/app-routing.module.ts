7d31554931701f4478b64f105ffeca204ec4e5c2
Wenceslao Waessle <wenceslao-10@hotmail.com>
2023-11-13 20:43:45 -0300

    Base template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './modules/landing/components/landing-page/landing-page.component';
+import { Error404Component } from './shared/components/error404/error404.component';
 
 const routes: Routes = [
   {
-    path:'landing',
-    component : LandingPageComponent,
-    loadChildren: ()=>import("./modules/landing/landing.module").then(m => m.LandingModule)
-},
-{
-    path:'',
-    redirectTo:'landing',
-    pathMatch:'full'
-}
+      path:'landing',
+      component : LandingPageComponent,
+      loadChildren: ()=>import("./modules/landing/landing.module").then(m => m.LandingModule)
+  },
+  {
+    path:'auth',
+    loadChildren: ()=>import("./modules/auth/auth.module").then(m => m.AuthModule)
+  },
+  {
+      path:'',
+      redirectTo:'landing',
+      pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:Error404Component
+  }
 ];
 
 @NgModule({
